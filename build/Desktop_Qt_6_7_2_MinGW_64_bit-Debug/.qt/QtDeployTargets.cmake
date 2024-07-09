e03d5f246f1bbdc41e7f9ba7ede3f8ae953706de
aaaMsLIUfruit <150137817+aaaMsLIUfruit@users.noreply.github.com>
2024-07-09 08:38:06 +0800

    Delete build directory

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_hello_FILE C:/Users/lenovo/Desktop/Tomato/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/hello.exe)
-set(__QT_DEPLOY_TARGET_hello_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_hello_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
